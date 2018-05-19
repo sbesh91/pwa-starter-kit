86da275d5f6bec1a2aeeea746481fe512b6da57f
sbesh91 <s.beshensky@gmail.com>
2018-05-18 20:15:36 -0500

    fix formatting issues

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -29,30 +29,30 @@ import { store } from '../store.js';
 import { navigate, updateOffline, updateDrawerState, updateLayout, ROUTES, ROUTE_NOT_FOUND } from '../actions/app.js';
 
 class MyApp extends connect(store)(LitElement) {
-  _render({ appTitle, _page, _drawerOpened, _snackbarOpened, _offline }) {
+  _render({appTitle, _page, _drawerOpened, _snackbarOpened, _offline}) {
     // Anything that's related to rendering should be done in here.
     return html`
     <style>
       :host {
         --app-drawer-width: 256px;
         display: block;
-    
+
         --app-primary-color: #E91E63;
         --app-secondary-color: #293237;
         --app-dark-text-color: var(--app-secondary-color);
         --app-light-text-color: white;
         --app-section-even-color: #f7f7f7;
         --app-section-odd-color: white;
-    
+
         --app-header-background-color: white;
         --app-header-text-color: var(--app-dark-text-color);
         --app-header-selected-color: var(--app-primary-color);
-    
+
         --app-drawer-background-color: var(--app-secondary-color);
         --app-drawer-text-color: var(--app-light-text-color);
         --app-drawer-selected-color: #78909C;
       }
-    
+
       app-header {
         position: fixed;
         top: 0;
@@ -63,38 +63,38 @@ class MyApp extends connect(store)(LitElement) {
         color: var(--app-header-text-color);
         border-bottom: 1px solid #eee;
       }
-    
+
       .toolbar-top {
         background-color: var(--app-header-background-color);
       }
-    
+
       [main-title] {
         font-family: 'Pacifico';
         text-transform: lowercase;
         font-size: 30px;
         /* In the narrow layout, the toolbar is offset by the width of the
-                    drawer button, and the text looks not centered. Add a padding to
-                    match that button */
+        drawer button, and the text looks not centered. Add a padding to
+        match that button */
         padding-right: 44px;
       }
-    
+
       .toolbar-list {
         display: none;
       }
-    
-      .toolbar-list>a {
+
+      .toolbar-list > a {
         display: inline-block;
         color: var(--app-header-text-color);
         text-decoration: none;
         line-height: 30px;
         padding: 4px 24px;
       }
-    
-      .toolbar-list>a[selected] {
+
+      .toolbar-list > a[selected] {
         color: var(--app-header-selected-color);
         border-bottom: 4px solid var(--app-header-selected-color);
       }
-    
+
       .menu-btn {
         background: none;
         border: none;
@@ -103,7 +103,7 @@ class MyApp extends connect(store)(LitElement) {
         height: 44px;
         width: 44px;
       }
-    
+
       .drawer-list {
         box-sizing: border-box;
         width: 100%;
@@ -112,77 +112,76 @@ class MyApp extends connect(store)(LitElement) {
         background: var(--app-drawer-background-color);
         position: relative;
       }
-    
-      .drawer-list>a {
+
+      .drawer-list > a {
         display: block;
         text-decoration: none;
         color: var(--app-drawer-text-color);
         line-height: 40px;
         padding: 0 24px;
       }
-    
-      .drawer-list>a[selected] {
+
+      .drawer-list > a[selected] {
         color: var(--app-drawer-selected-color);
       }
-    
+
       .main-content {
         padding-top: 64px;
         min-height: 100vh;
       }
-    
+
       .page {
         display: none;
       }
-    
+
       .page[active] {
         display: block;
       }
-    
+
       footer {
         padding: 24px;
         background: var(--app-drawer-background-color);
         color: var(--app-drawer-text-color);
         text-align: center;
       }
-    
+
       /* Wide layout: when the viewport width is bigger than 460px, layout
-                  changes to a wide layout. */
-    
+      changes to a wide layout. */
       @media (min-width: 460px) {
         .toolbar-list {
           display: block;
         }
-    
+
         .menu-btn {
           display: none;
         }
-    
+
         .main-content {
           padding-top: 107px;
         }
-    
+
         /* The drawer button isn't shown in the wide layout, so we don't
-                    need to offset the title */
+        need to offset the title */
         [main-title] {
           padding-right: 0px;
         }
       }
     </style>
-    
+
     <!-- Header -->
     <app-header condenses reveals effects="waterfall">
       <app-toolbar class="toolbar-top">
         <button class="menu-btn" title="Menu" on-click="${_ => store.dispatch(updateDrawerState(true))}">${menuIcon}</button>
         <div main-title>${appTitle}</div>
       </app-toolbar>
-    
+
       <!-- This gets hidden on a small screen-->
       <nav class="toolbar-list">
         ${ROUTES.map(i => html`
         <a selected?="${_page === i.key}" href="${i.href}">${i.title}</a>`)}
       </nav>
     </app-header>
-    
+
     <!-- Drawer content -->
     <app-drawer opened="${_drawerOpened}" on-opened-changed="${e => store.dispatch(updateDrawerState(e.target.opened))}">
       <nav class="drawer-list">
@@ -190,14 +189,12 @@ class MyApp extends connect(store)(LitElement) {
         <a selected?="${_page === i.key}" href="${i.href}">${i.title}</a>`)}
       </nav>
     </app-drawer>
-    
+
     <!-- Main content -->
     <main class="main-content">   
       ${this.getActiveView(_page)}
     </main>
       
-    
-    
     <footer>
       <p>Made with &lt;3 by the Polymer team.</p>
     </footer>
